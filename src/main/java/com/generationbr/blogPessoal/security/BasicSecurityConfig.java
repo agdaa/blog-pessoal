2e1fee42bef0a0fe6bcf40d02e1095efc57201f7
agdaa <agda.camilla@gmail.com>
2021-08-09 21:19:56 -0300

    [Correção de autorização]

--- src/main/java/com/generationbr/blogPessoal/security/BasicSecurityConfig.java
+++ src/main/java/com/generationbr/blogPessoal/security/BasicSecurityConfig.java
@@ -34,6 +34,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
  	protected void configure(HttpSecurity http) throws Exception {
 		
  		http.authorizeRequests()
+ 		.antMatchers("/**").permitAll()
  		.antMatchers(HttpMethod.POST,"/usuario/login").permitAll()
  		.antMatchers(HttpMethod.POST,"/usuario/cadastrar").permitAll()
  		.anyRequest().authenticated()
